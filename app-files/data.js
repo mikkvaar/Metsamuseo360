ef20a1290855b02db6b678c711aa315654c78c9f
Mikko Vaarala <mikko.vaarala@edu.lapinamk.fi>
2024-05-22 12:09:10 +0300

    added marzipano demo

---
+++ app-files/data.js
@@ -0,0 +1,258 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lighthouse_pano_left",
+      "name": "lighthouse_pano_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1936211182286662,
+          "pitch": -0.011554921226903048,
+          "rotation": 0,
+          "target": "4-lighthouse5_pano_left"
+        },
+        {
+          "yaw": 0.24187451176584318,
+          "pitch": -0.014366942002460803,
+          "rotation": 0,
+          "target": "3-lighthouse4_pano_left"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8744540294732523,
+          "pitch": -0.41741772353499584,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6442715204932341,
+          "pitch": -0.15940556296771646,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.877172709126034,
+          "pitch": -0.07297508134211128,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-lighthouse2_pano_left",
+      "name": "lighthouse2_pano_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8607436295183426,
+          "pitch": -0.2293166207896924,
+          "rotation": 0,
+          "target": "3-lighthouse4_pano_left"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lighthouse3_pano_left",
+      "name": "lighthouse3_pano_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1172031105182825,
+          "pitch": 0.06296651180374369,
+          "rotation": 0,
+          "target": "1-lighthouse2_pano_left"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9956801110900866,
+          "pitch": -0.9345173219839271,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-lighthouse4_pano_left",
+      "name": "lighthouse4_pano_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1329723478768496,
+          "pitch": 0.18311473651793975,
+          "rotation": 0,
+          "target": "4-lighthouse5_pano_left"
+        },
+        {
+          "yaw": -0.11329103149539499,
+          "pitch": 0.16744700233157772,
+          "rotation": 0,
+          "target": "2-lighthouse3_pano_left"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17520898110365124,
+          "pitch": -0.04761914394065414,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0479640098578304,
+          "pitch": 0.0018734236970914253,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9823829064978709,
+          "pitch": -0.010545581952900562,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-lighthouse5_pano_left",
+      "name": "lighthouse5_pano_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43370238974144826,
+          "pitch": -0.02917224531258178,
+          "rotation": 0,
+          "target": "3-lighthouse4_pano_left"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
