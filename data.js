48019de84d4869ab84766533520d1d31f3360ddf
Mikko Vaarala <mikko.vaarala@edu.lapinamk.fi>
2024-05-22 13:05:14 +0300

    added different placeholders and different hotspots

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-lighthouse_pano_left",
-      "name": "lighthouse_pano_left",
+      "id": "0-placeholder",
+      "name": "Placeholder",
       "levels": [
         {
           "tileSize": 256,
@@ -24,48 +24,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.21883743698082903,
+        "pitch": -0.03383562288180997,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -1.1936211182286662,
-          "pitch": -0.011554921226903048,
+          "yaw": 0.17114807528763976,
+          "pitch": 0.023257073825782015,
           "rotation": 0,
-          "target": "4-lighthouse5_pano_left"
+          "target": "1-placeholder2"
         },
         {
-          "yaw": 0.24187451176584318,
-          "pitch": -0.014366942002460803,
+          "yaw": -1.2039930514758872,
+          "pitch": -0.019723214392854516,
           "rotation": 0,
-          "target": "3-lighthouse4_pano_left"
+          "target": "4-placeholder5"
         }
       ],
       "infoHotspots": [
-        {
-          "yaw": -0.8744540294732523,
-          "pitch": -0.41741772353499584,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": 0.6442715204932341,
-          "pitch": -0.15940556296771646,
-          "title": "Title",
-          "text": "Text"
-        },
         {
           "yaw": -2.877172709126034,
           "pitch": -0.07297508134211128,
-          "title": "Title",
-          "text": "Text"
+          "title": "The standard Lorem Ipsum passage",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
       ]
     },
     {
-      "id": "1-lighthouse2_pano_left",
-      "name": "lighthouse2_pano_left",
+      "id": "1-placeholder2",
+      "name": "Placeholder2",
       "levels": [
         {
           "tileSize": 256,
@@ -87,68 +75,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.6554422255669516,
+        "pitch": 0.11466516643280045,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -2.8607436295183426,
-          "pitch": -0.2293166207896924,
+          "yaw": 0.2747633949030952,
+          "pitch": 0.23038258583891036,
           "rotation": 0,
-          "target": "3-lighthouse4_pano_left"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-lighthouse3_pano_left",
-      "name": "lighthouse3_pano_left",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "target": "2-placeholder3"
         },
         {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1920,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.1172031105182825,
-          "pitch": 0.06296651180374369,
+          "yaw": 3.073475589477865,
+          "pitch": 0.1810453752040413,
           "rotation": 0,
-          "target": "1-lighthouse2_pano_left"
+          "target": "0-placeholder"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -1.9956801110900866,
-          "pitch": -0.9345173219839271,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 1.6820615742665401,
+          "pitch": 0.050339434504419245,
+          "title": "Lorem ipsum dolor sit amet",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
       ]
     },
     {
-      "id": "3-lighthouse4_pano_left",
-      "name": "lighthouse4_pano_left",
+      "id": "2-placeholder3",
+      "name": "Placeholder3",
       "levels": [
         {
           "tileSize": 256,
@@ -170,48 +126,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.09497371392744114,
+        "pitch": -0.23753290810967265,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -3.1329723478768496,
-          "pitch": 0.18311473651793975,
+          "yaw": -2.8358635101542085,
+          "pitch": -0.2699319245268885,
           "rotation": 0,
-          "target": "4-lighthouse5_pano_left"
+          "target": "1-placeholder2"
         },
         {
-          "yaw": -0.11329103149539499,
-          "pitch": 0.16744700233157772,
+          "yaw": -1.4684438483035578,
+          "pitch": -0.07509496051018694,
           "rotation": 0,
-          "target": "2-lighthouse3_pano_left"
+          "target": "3-placeholder4"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.17520898110365124,
-          "pitch": -0.04761914394065414,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": 1.0479640098578304,
-          "pitch": 0.0018734236970914253,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": 1.9823829064978709,
-          "pitch": -0.010545581952900562,
-          "title": "Title",
-          "text": "Text"
+          "yaw": -0.18259601030804262,
+          "pitch": -0.07222778133794705,
+          "title": "Lorem ipsum dolor sit amet",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         }
       ]
     },
     {
-      "id": "4-lighthouse5_pano_left",
-      "name": "lighthouse5_pano_left",
+      "id": "3-placeholder4",
+      "name": "Placeholder4",
       "levels": [
         {
           "tileSize": 256,
@@ -233,26 +177,78 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.5489283582822004,
+        "pitch": 0.07016162355753508,
+        "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -0.43370238974144826,
-          "pitch": -0.02917224531258178,
+          "yaw": -3.1220162765148594,
+          "pitch": 0.0775223855942091,
           "rotation": 0,
-          "target": "3-lighthouse4_pano_left"
+          "target": "2-placeholder3"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 0.17483260118168786,
+          "pitch": 0.092212857297433,
+          "title": "Lorem ipsum dolor sit amet",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "4-placeholder5",
+      "name": "Placeholder5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.3034077354947087,
+        "pitch": -0.7831902878611956,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.35334233856607433,
+          "pitch": 0.11513050944559922,
+          "rotation": 0,
+          "target": "0-placeholder"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4613352183552166,
+          "pitch": -0.7989806537119648,
+          "title": "Lorem ipsum dolor sit amet",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&nbsp;"
+        }
+      ]
     }
   ],
-  "name": "Project Title",
+  "name": "Metsämuseo idea",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
