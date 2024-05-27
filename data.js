9827662f0a2e00d106e3609cc7f13e1107859550
Mikko Vaarala <mikko.vaarala@edu.lapinamk.fi>
2024-05-27 11:25:07 +0300

    changed test pictures.

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-placeholder",
-      "name": "Placeholder",
+      "id": "0-lapit",
+      "name": "lapIT",
       "levels": [
         {
           "tileSize": 256,
@@ -24,36 +24,30 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": 0.21883743698082903,
-        "pitch": -0.03383562288180997,
+        "yaw": -2.4442281304852482,
+        "pitch": -0.18636446253949224,
         "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": 0.17114807528763976,
-          "pitch": 0.023257073825782015,
+          "yaw": 2.1714642059894036,
+          "pitch": 0.025462361005187617,
           "rotation": 0,
-          "target": "1-placeholder2"
-        },
-        {
-          "yaw": -1.2039930514758872,
-          "pitch": -0.019723214392854516,
-          "rotation": 0,
-          "target": "4-placeholder5"
+          "target": "1-amk"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -2.877172709126034,
-          "pitch": -0.07297508134211128,
-          "title": "The standard Lorem Ipsum passage",
-          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          "yaw": -2.352527773573014,
+          "pitch": -0.44761625956570406,
+          "title": "LapIT Oy",
+          "text": "LapIT Oy on pohjoissuomalainen ICT -infra ja perustietotekniikan palveluita omistajilleen tuottava in-house -yhtiö."
         }
       ]
     },
     {
-      "id": "1-placeholder2",
-      "name": "Placeholder2",
+      "id": "1-amk",
+      "name": "AMK",
       "levels": [
         {
           "tileSize": 256,
@@ -75,36 +69,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": 1.6554422255669516,
-        "pitch": 0.11466516643280045,
+        "yaw": 0.6994755494295823,
+        "pitch": -0.2932420649756864,
         "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": 0.2747633949030952,
-          "pitch": 0.23038258583891036,
+          "yaw": -0.08558909430939465,
+          "pitch": 0.007690433737938207,
           "rotation": 0,
-          "target": "2-placeholder3"
+          "target": "0-lapit"
         },
         {
-          "yaw": 3.073475589477865,
-          "pitch": 0.1810453752040413,
+          "yaw": 2.9997704480377516,
+          "pitch": 0.05190247869898457,
           "rotation": 0,
-          "target": "0-placeholder"
+          "target": "2-scootti"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 1.6820615742665401,
-          "pitch": 0.050339434504419245,
-          "title": "Lorem ipsum dolor sit amet",
-          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          "yaw": 0.5976776769115943,
+          "pitch": -0.4377547711498515,
+          "title": "Lapin ammattikorkeakoulu",
+          "text": "Lapin ammattikorkeakoulu on suomalainen ammattikorkeakoulu, joka aloitti toimintansa vuoden 2014 alussa, kun Kemi-Tornion ammattikorkeakoulu ja Rovaniemen ammattikorkeakoulu lakkautettiin ja niiden toiminnat yhdistettiin."
         }
       ]
     },
     {
-      "id": "2-placeholder3",
-      "name": "Placeholder3",
+      "id": "2-scootti",
+      "name": "scootti",
       "levels": [
         {
           "tileSize": 256,
@@ -126,36 +120,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": -0.09497371392744114,
-        "pitch": -0.23753290810967265,
+        "yaw": -1.3119945616064488,
+        "pitch": 0.028444286005985475,
         "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -2.8358635101542085,
-          "pitch": -0.2699319245268885,
+          "yaw": -2.8525368354046687,
+          "pitch": 0.015238235812091716,
           "rotation": 0,
-          "target": "1-placeholder2"
+          "target": "3-klapimaatti"
         },
         {
-          "yaw": -1.4684438483035578,
-          "pitch": -0.07509496051018694,
+          "yaw": 0.30967733038864154,
+          "pitch": 0.05938190515343145,
           "rotation": 0,
-          "target": "3-placeholder4"
+          "target": "1-amk"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.18259601030804262,
-          "pitch": -0.07222778133794705,
-          "title": "Lorem ipsum dolor sit amet",
-          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          "yaw": -1.4266450188411106,
+          "pitch": 0.2086010906694291,
+          "title": "JoeScooter",
+          "text": "Joescooter on uusi tapa kulkea. JOE:lla ajaminen on hauskaa, vaivatonta ja ympäristöystävällistä."
         }
       ]
     },
     {
-      "id": "3-placeholder4",
-      "name": "Placeholder4",
+      "id": "3-klapimaatti",
+      "name": "Klapimaatti",
       "levels": [
         {
           "tileSize": 256,
@@ -177,30 +171,36 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": 0.5489283582822004,
-        "pitch": 0.07016162355753508,
+        "yaw": 1.7734542653258174,
+        "pitch": -0.023946827471339915,
         "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": -3.1220162765148594,
-          "pitch": 0.0775223855942091,
+          "yaw": -2.214979940896397,
+          "pitch": 0.000241530569569548,
           "rotation": 0,
-          "target": "2-placeholder3"
+          "target": "4-joki"
+        },
+        {
+          "yaw": 0.891232949026822,
+          "pitch": 0.025787262875104133,
+          "rotation": 0,
+          "target": "2-scootti"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 0.17483260118168786,
-          "pitch": 0.092212857297433,
-          "title": "Lorem ipsum dolor sit amet",
-          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&nbsp;"
+          "yaw": 1.450040109894056,
+          "pitch": 0.014728850899798474,
+          "title": "Klapimaatti<br>",
+          "text": "<div>Ajatus Klapimaatin eli älykkään halkoautomaatin toteutukseen lähti halkoja laavuille toimittavien tahojen aloitteesta; laavujen puita poltettiin tuhlaillen ja jopa varastettiin. Tästä syystä tavallinen tulistelija saattoikin tulla laavulle, jossa ei ollut jäljellä enää klapin klapia poltettavaksi.</div><div><br></div><div>Laavuille haluttiin keksiä sen käyttäjiä palveleva automaattiratkaisu, joka veloittaa pienen summan puista, ja takaa sen, että jokaiselle laavulle saapujalle on tarjolla puita makkaranpaistoa, kuivattelua, lämmittelyä tai vaikka fiilistelyä varten.</div>"
         }
       ]
     },
     {
-      "id": "4-placeholder5",
-      "name": "Placeholder5",
+      "id": "4-joki",
+      "name": "joki",
       "levels": [
         {
           "tileSize": 256,
@@ -222,30 +222,29 @@ var APP_DATA = {
       ],
       "faceSize": 1920,
       "initialViewParameters": {
-        "yaw": -1.3034077354947087,
-        "pitch": -0.7831902878611956,
+        "yaw": -2.2045589036171087,
+        "pitch": 0.13344613492843926,
         "fov": 1.3365071038314758
       },
       "linkHotspots": [
         {
-          "yaw": 0.35334233856607433,
-          "pitch": 0.11513050944559922,
+          "yaw": 1.0083533231669701,
+          "pitch": 0.06004403469405872,
           "rotation": 0,
-          "target": "0-placeholder"
+          "target": "3-klapimaatti"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -1.4613352183552166,
-          "pitch": -0.7989806537119648,
-          "title": "Lorem ipsum dolor sit amet",
-          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&nbsp;"
+          "yaw": -2.399120811141689,
+          "pitch": 0.044010161463797104,
+          "title": "Kemijoki",
+          "text": "Kemijoki on Suomen pisin joki, pituutta sillä on 550 kilometriä. Joki laskee Kemijärven ja Rovaniemen kautta Pohjanlahteen Kemin ja Keminmaan rajalla.<br>"
         }
       ]
     }
-    
   ],
-  "name": "Metsämuseo idea",
+  "name": "Metsamuseodemo",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
